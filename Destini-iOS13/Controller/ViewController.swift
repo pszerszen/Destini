3c159bbf199256c780509b858e2e6d5d5a7440d1
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-03 11:00:27 +0100

    initial commit

---
+++ Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        updateUI()
+    }
+    
+    @IBAction func choiceButtonPressed(_ sender: UIButton) {
+        storyBrain.progressStory(usersChoice: sender.currentTitle!)
+        
+        sender.backgroundColor = UIColor.lightGray
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        storyLabel.text = storyBrain.getTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        
+        [choice1Button, choice2Button].forEach { button in
+            button?.backgroundColor = UIColor.clear
+        }
+    }
+
+}
+
